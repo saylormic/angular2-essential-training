54d3e493013c5a36ad4efa22b5219ca1e426f5e7
Michael Saylor <saylormic@gmail.com>
2018-08-28 15:34:30 -0400

    followed video instructions

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
 
 import { MediaItemService } from './media-item.service';
@@ -13,7 +13,8 @@ export class MediaItemFormComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private mediaItemService: MediaItemService) {}
+    private mediaItemService: MediaItemService,
+    @Inject('lookupListToken') public lookupLists) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
