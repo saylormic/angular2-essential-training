7eff0f8dc22fafde1f765b432f6e561eeae74bea
Michael Saylor <saylormic@gmail.com>
2018-08-28 19:05:35 -0400

    followed video instructions

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
+import { MockXHRBackend } from './mock-xhr-backend';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -28,7 +29,8 @@ import { lookupListToken, lookupLists } from './providers';
   ],
   providers: [
     MediaItemService,
-    { provide: lookupListToken, useValue: lookupLists }
+    { provide: lookupListToken, useValue: lookupLists },
+    { provide: HttpXhrBackend, useClass: MockXHRBackend }
   ],
   bootstrap: [
     AppComponent
