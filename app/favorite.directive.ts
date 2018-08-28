1145e2f6f504c011eba1f03291a442c2d9f4afab
Michael Saylor <saylormic@gmail.com>
2018-08-28 10:14:59 -0400

    followed video instructions

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -1,10 +1,21 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+
+  @HostBinding('class.is-favorite-hovering') hovering = true;
+  
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.hovering = false;
+  }
+
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
