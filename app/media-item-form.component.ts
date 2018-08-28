7980069f631686673d332953ac9fbd879b67c8d4
Michael Saylor <saylormic@gmail.com>
2018-08-28 10:51:39 -0400

    followed video instructions
    custom form validator

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -17,8 +17,23 @@ export class MediaItemFormComponent {
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
+
+  yearValidator(control) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+
+    let year = parseInt(control.value);
+    let minYear = 1900;
+    let maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { 'year': true };
+    }
   }
 
   onSubmit(mediaItem) {
