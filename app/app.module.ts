550d755fc22f0e8326b5c4c5985364c89811f3d8
Michael Saylor <saylormic@gmail.com>
2018-08-28 10:35:20 -0400

    followed video instructons
    Template-driven form

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -10,7 +11,8 @@ import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
